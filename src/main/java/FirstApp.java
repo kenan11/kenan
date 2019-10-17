7a95c33c0eac5bc27e15c899a0fc6fe5a01b3819
User 2017 <kenansultanov54@gmail.com>
2019-10-17 10:07:32 +0400

    -- deyisiklik -- --

--- src/main/java/FirstApp.java
+++ src/main/java/FirstApp.java
@@ -1,7 +1,7 @@
 public class FirstApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Ukraine!");
+        System.out.println("Hllo from Ukraine!");
     }
 
 
---
+++ src/main/java/HomeWorkOne.java
@@ -0,0 +1,5 @@
+public class HomeWorkOne {
+    public static void main(String[] args) {
+
+    }
+}
