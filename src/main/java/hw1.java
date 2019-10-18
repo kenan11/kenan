6aa206286e03e7f34b05994d94bf09ec55ad5ab5
User 2017 <kenansultanov54@gmail.com>
2019-10-18 23:08:29 +0400

    -- first -- --

--- src/main/java/hw1.java
+++ src/main/java/hw1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class hw1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random rb = new Random();
+        Random rb = new  Random();
         System.out.print("Please enter your name :");
         String name = sc.nextLine();
         System.out.println("Let the game begin!");
