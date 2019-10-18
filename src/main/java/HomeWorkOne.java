182f39151a3534865c15ae354f763736c2872124
User 2017 <kenansultanov54@gmail.com>
2019-10-18 23:07:21 +0400

    -- first -- --

--- src/main/java/HomeWorkOne.java
+++ src/main/java/HomeWorkOne.java
@@ -1,5 +1,5 @@
 public class HomeWorkOne {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("*");
     }
 }
---
+++ src/main/java/hw1.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class hw1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rb = new Random();
+        System.out.print("Please enter your name :");
+        String name = sc.nextLine();
+        System.out.println("Let the game begin!");
+        int number = rb.nextInt(100);
+
+        while(true) {
+            System.out.print("Please enter your number :");
+            int your_number = sc.nextInt();
+
+            if(your_number > number) {
+                System.out.print("Your number is too big. Please , try again.");
+            }
+            else if(your_number < number) {
+                System.out.println("Your number is too small. Please , try again.");
+            }
+            else  {
+                System.out.println("Congratulations,"+name);
+                break;
+            }
+        }
+
+
+
+    }
+}
